c678210020044b6f6d086eb6d5b8a7f3bc69202d
joyce <joycefrontend@gmail.com>
2022-01-01 13:57:38 -0300

    incremento

---
+++ javascript-basico/14-incremento.js
@@ -0,0 +1,17 @@
+/* Incrementar um número com JavaScript */
+
+/* Você pode facilmente incrementar ou adicionar 1 à variável com o operador ++.
+
+i++;
+é o equivalente a
+
+i = i + 1;
+Observação: a linha inteira torna-se i++;, eliminando a necessidade para o sinal de igual (atribuição).
+
+*/
+
+//Exemplo:
+
+let myVar = 87;
+myVar ++;  // "+ 1"
+console.log(myVar); // 88
